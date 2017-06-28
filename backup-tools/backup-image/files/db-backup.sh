893ac992d5c4e96d70d9bbe795cdacf793332a2a
Josef Fröhle <github@josef-froehle.de>
2017-06-28 11:20:16 +0200

    correct mysql backup

--- backup-tools/backup-image/files/db-backup.sh
+++ backup-tools/backup-image/files/db-backup.sh
@@ -59,9 +59,9 @@ if [ "$BACKUP_MYSQL_FULL" ] && [ "$BACKUP_MYSQL_INCR" ] && [ -S $MYSQL_SOCKET ] 
 
     # MySQL Backup (full)
     #xtrabackup --backup --user=$USERNAME --password=$PASSWORD --target-dir=$BACKUP_BASE --datadir=$DATADIR --socket=$MYSQL_SOCKET
-    for i in `mysql --user=$USERNAME --password=$PASSWORD --socket=$MYSQL_SOCKET -e 'show databases' | awk '{print $1}' | grep -E -v "(^Database$|^information_schema$|^performance_schema$)"`; do
+    for i in `mysql --user=$USERNAME --password=$PASSWORD --socket=$MYSQL_SOCKET -e 'show databases' | awk '{print $1}' | grep -E -v "(^Database$|^information_schema$|^sys$|^mysql$|^performance_schema$)"`; do
       echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Creating backup of database $i ..."
-      mysqldump --user=$USERNAME --password=$PASSWORD --socket=$MYSQL_SOCKET $i > $BACKUP_BASE/$i.sql
+      mysqldump --opt --user=$USERNAME --password=$PASSWORD --socket=$MYSQL_SOCKET --databases $i > $BACKUP_BASE/$i.sql
       gzip -f $BACKUP_BASE/$i.sql
     done
     echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Uploading backup to $SWIFT_CONTAINER/$CUR_TS ..."
@@ -93,10 +93,12 @@ if [ "$BACKUP_PGSQL_FULL" ] ; then
     if [ "$PG_DUMP" = 1 ] ; then
       for i in `psql -q -A -t -c "SELECT datname FROM pg_database" -h localhost -U postgres | grep -E -v "(^template|^postgres$)"` ; do
         echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Creating backup of database $i ..."
-        pg_dump -U postgres -h localhost $i --file=$BACKUP_BASE/$i.sql.gz -Z 5
+        pg_dump -U postgres -h localhost -c --if-exist -C $i --file=$BACKUP_BASE/$i.sql.gz -Z 5
       done
-      echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Uploading backup to $SWIFT_CONTAINER/$CUR_TS ..."
-      swift upload --header "X-Delete-After: $BACKUP_EXPIRE_AFTER" --changed "$SWIFT_CONTAINER/$CUR_TS" $BACKUP_BASE
+      if [ -s "$SWIFT_CONTAINER/$CUR_TS" ] ; then
+        echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Uploading backup to $SWIFT_CONTAINER/$CUR_TS ..."
+        swift upload --header "X-Delete-After: $BACKUP_EXPIRE_AFTER" --changed "$SWIFT_CONTAINER/$CUR_TS" $BACKUP_BASE
+      fi
     else
       # Postgres Backup (full)
       /usr/bin/barman  cron
@@ -110,35 +112,35 @@ if [ "$BACKUP_PGSQL_FULL" ] ; then
   fi
 fi
 
-if [ "$BACKUP_INFLUXDB_FULL" ] ; then
-  BACKUP_BASE=/backup/influxdb/base
-  USERNAME=root
-  PASSWORD=$INFLUXDB_ROOT_PASSWORD
-
-  if [ ! -d "$BACKUP_BASE" ] ; then
-    mkdir -p "$BACKUP_BASE"
-  fi
-
-  INTERVAL_FULL="$BACKUP_INFLUXDB_FULL"
-  IS_NEXT_TS_FULL="$(date --date="now - $INTERVAL_FULL" +%Y%m%d%H%M)"
-
-  if [ "$IS_NEXT_TS_FULL" -ge "$LAST_BACKUP_TS" ] ; then
-    echo $$ > $PIDFILE
-    echo "$CUR_TS" > $LAST_BACKUP_FILE
-
-    for i in `influx -username "$USERNAME" -password "$PASSWORD" -execute 'show databases' -host localhost -port 8086 | grep -E -v "(^---|^_internal|^name|^$)"` ; do
-      echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Creating backup of database $i ..."
-      influxd backup -database $i -host localhost:8088 "$BACKUP_BASE/$i"
-      tar zcvf "$BACKUP_BASE/$i.tar.gz" "$BACKUP_BASE/$i"
-      rm -rf $BACKUP_BASE/$i
-    done
-    echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Uploading backup to influxdb/$SWIFT_CONTAINER/$CUR_TS ..."
-    swift upload --header "X-Delete-After: $BACKUP_EXPIRE_AFTER" --changed "$SWIFT_CONTAINER/$CUR_TS" $BACKUP_BASE
-
-    swift upload $SWIFT_CONTAINER$LAST_BACKUP_FILE $LAST_BACKUP_FILE
-    rm -f $LAST_BACKUP_FILE
-  fi
-fi
+#if [ "$BACKUP_INFLUXDB_FULL" ] ; then
+#  BACKUP_BASE=/backup/influxdb/base
+#  USERNAME=root
+#  PASSWORD=$INFLUXDB_ROOT_PASSWORD
+#
+#  if [ ! -d "$BACKUP_BASE" ] ; then
+#    mkdir -p "$BACKUP_BASE"
+#  fi
+#
+#  INTERVAL_FULL="$BACKUP_INFLUXDB_FULL"
+#  IS_NEXT_TS_FULL="$(date --date="now - $INTERVAL_FULL" +%Y%m%d%H%M)"
+#
+#  if [ "$IS_NEXT_TS_FULL" -ge "$LAST_BACKUP_TS" ] ; then
+#    echo $$ > $PIDFILE
+#    echo "$CUR_TS" > $LAST_BACKUP_FILE
+#
+#    for i in `influx -username "$USERNAME" -password "$PASSWORD" -execute 'show databases' -host localhost -port 8086 | grep -E -v "(^---|^_internal|^name|^$)"` ; do
+#      echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Creating backup of database $i ..."
+#      influxd backup -database $i -host localhost:8088 "$BACKUP_BASE/$i"
+#      tar zcvf "$BACKUP_BASE/$i.tar.gz" "$BACKUP_BASE/$i"
+#      rm -rf $BACKUP_BASE/$i
+#    done
+#    echo "$(date +'%Y/%m/%d %H:%M:%S %Z') Uploading backup to influxdb/$SWIFT_CONTAINER/$CUR_TS ..."
+#    swift upload --header "X-Delete-After: $BACKUP_EXPIRE_AFTER" --changed "$SWIFT_CONTAINER/$CUR_TS" $BACKUP_BASE
+#
+#    swift upload $SWIFT_CONTAINER$LAST_BACKUP_FILE $LAST_BACKUP_FILE
+#    rm -f $LAST_BACKUP_FILE
+#  fi
+#fi
 
 rm -f $PIDFILE
 exit 0
